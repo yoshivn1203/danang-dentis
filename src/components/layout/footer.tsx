1525736522d6a3c4cf0d2d492e02a7f7dc1ddd84
Tung Nguyen <tung.nguyen@scopicsoftware.com>
2025-02-26 09:40:48 +0700

    update email address

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -82,7 +82,7 @@ export function Footer() {
                   </div>
                   <div className='flex items-center gap-2'>
                     <Mail className='h-4 w-4' />
-                    <span>contact@danangdentists.com</span>
+                    <span>contact@danangdentalcare.com</span>
                   </div>
                   <div className='flex items-center gap-2'>
                     <MapPin className='h-4 w-4' />
